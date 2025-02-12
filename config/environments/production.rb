950f506c5e6d18569a6a6e1f3675082413b91fc3
Juraj Matase <jurajmatase@Mac-mini-uzivatela-Juraj.local>
2025-02-12 21:32:54 +0100

    changing port

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.m1.websupport.sk',
-    port: 465,
+    port: 587,
     domain: 'tutuma.matase.sk',
     user_name: Rails.application.credentials.dig(:smtp, :user_name),
     password: Rails.application.credentials.dig(:smtp, :password),
