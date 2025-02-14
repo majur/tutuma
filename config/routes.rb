3d691ec4c4cef21b1808fa431080fbea22d0a387
Juraj Matase <jurajmatase@Mac-mini-uzivatela-Juraj.local>
2025-02-14 16:07:26 +0100

    Refactoring routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,20 @@
 Rails.application.routes.draw do
-  root to: "users#index"
-  get "up" => "rails/health#show", as: :rails_health_check
-  resource :session
-  resources :passwords, param: :token, only: [ :new, :create, :edit, :update ]
-  get "/registration", to: "registrations#new", as: :new_registration
-  post "/registration", to: "registrations#create"
-  resources :invitations, only: [ :new, :create ]
-  get "/invitations/accept", to: "invitations#edit", as: :edit_invitation
-  patch "/invitations/accept/:token", to: "invitations#update", as: :update_invitation
-  delete "/logout", to: "sessions#destroy", as: :logout
-  resources :users, only: [ :index ]
+  root "users#index"
+
+  get "up", to: "rails/health#show", as: :rails_health_check
+
+  resource :session, only: [:new, :create, :destroy]
+  resources :passwords, param: :token, only: [:new, :create, :edit, :update]
+  resources :invitations, only: [:new, :create] do
+    collection do
+      get "accept", to: "invitations#edit", as: :edit
+      patch "accept/:token", to: "invitations#update", as: :update
+    end
+  end
+
+  get "registration", to: "registrations#new", as: :new_registration
+  post "registration", to: "registrations#create"
+  delete "logout", to: "sessions#destroy", as: :logout
+
+  resources :users, only: [:index]
 end
