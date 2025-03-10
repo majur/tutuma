847d7f048532f434a332b4294028ab5068a4f65c
Juraj Matase <jurajmatase@Mac-mini-uzivatela-Juraj.local>
2025-03-10 19:02:25 +0100

    dva

---
+++ app/components/logout_button_component.rb
@@ -0,0 +1,10 @@
+class LogoutButtonComponent < ViewComponent::Base
+    def initialize(full_width: false, css_class: nil)
+      @full_width = full_width
+      @css_class = css_class
+    end
+  
+    private
+  
+    attr_reader :full_width, :css_class
+  end
---
+++ app/components/team_members_component.rb
@@ -0,0 +1,11 @@
+class TeamMembersComponent < ViewComponent::Base
+    def initialize(account:, users:, current_user:)
+      @account = account
+      @users = users 
+      @current_user = current_user
+    end
+  
+    private
+  
+    attr_reader :account, :users, :current_user
+  end 
